1d011201f00e85185233914cd0181ebd48e32991
MukibHossain <mukib.11990@gmail.com>
2026-05-16 14:38:38 +0600

    config- db

--- config/database.php
+++ config/database.php
@@ -1,4 +1,36 @@
 <?php
-$conn = new mysqli("localhost","root","","online_quiz_system");
+
+
+if(
+session_status()
+!== PHP_SESSION_ACTIVE
+){
+
 session_start();
-?>
+
+}
+
+
+
+$conn=
+
+new mysqli(
+
+"localhost",
+"root",
+"",
+"online_quiz_system"
+
+);
+
+
+
+if(
+$conn->connect_error
+){
+
+die(
+"Database Connection Failed"
+);
+
+}
