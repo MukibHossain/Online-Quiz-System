0e8df8a179c555735b6cc8ac3613dd5c14497299
MukibHossain <mukib.11990@gmail.com>
2026-05-17 01:16:16 +0600

    login code update

--- auth/login.php
+++ auth/login.php
@@ -1,5 +1,112 @@
 <?php require '../config/database.php'; ?>
 
+
+<?php
+
+if(isset($_POST['login'])){
+
+
+$email=
+$conn->real_escape_string(
+$_POST['email']
+);
+
+
+$password=
+$_POST['password'];
+
+
+
+$data=
+$conn->query(
+
+"SELECT * FROM users
+
+WHERE email='$email'"
+
+);
+
+
+
+if(
+$data->num_rows==0
+){
+
+echo
+"<script>
+
+alert('User not found');
+
+</script>";
+
+}
+else{
+
+
+$user=
+$data->fetch_assoc();
+
+
+
+if(
+
+password_verify(
+
+$password,
+
+$user['password']
+
+)
+
+){
+
+
+$_SESSION['user_id']=
+$user['id'];
+
+
+$_SESSION['role']=
+$user['role'];
+
+
+
+if(
+$user['role']=='admin'
+){
+
+header(
+"Location: ../admin/dashboard.php"
+);
+
+}
+else{
+
+header(
+"Location: ../student/dashboard.php"
+);
+
+}
+
+exit();
+
+}
+else{
+
+echo
+"<script>
+
+alert('Wrong password');
+
+</script>";
+
+}
+
+}
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -35,11 +142,11 @@ linear-gradient(
 
 45deg,
 
-rgba(15,23,42,0.45),
+rgba(15,23,42,.45),
 
-rgba(37,99,235,0.30),
+rgba(37,99,235,.30),
 
-rgba(147,51,234,0.30)
+rgba(147,51,234,.30)
 
 );
 
@@ -60,26 +167,11 @@ margin-top:70px;
 
 padding:40px;
 
-background:rgba(
-255,
-255,
-255,
-0.10
-);
+background:rgba(255,255,255,.10);
 
 backdrop-filter:blur(14px);
 
 border-radius:25px;
-
-color:white;
-
-box-shadow:0 0 30px rgba(0,0,0,.5);
-
-}
-
-a{
-
-text-decoration:none;
 
 color:white;
 
@@ -92,38 +184,19 @@ color:white;
 
 <body>
 
-
 <div class="overlay"></div>
 
-
 <div class="container">
-
 
 <div class="box">
 
 
-<div class="mb-3">
+<a href="../index.php" class="btn btn-info">🏠 Home</a>
+
+<a href="javascript:history.back()" class="btn btn-warning">← Back</a>
 
 
-<a
-href="../index.php"
-class="btn btn-info">
-
-🏠 Home
-
-</a>
-
-
-<a
-href="javascript:history.back()"
-class="btn btn-warning">
-
-← Back
-
-</a>
-
-
-</div>
+<br><br>
 
 
 <h1 class="text-center">
@@ -139,58 +212,21 @@ class="btn btn-warning">
 <form method="POST">
 
 
-<input
-name="email"
-class="form-control"
-placeholder="Email">
-
+<input name="email" class="form-control" placeholder="Email">
 
 <br>
 
 
-<input
-type="password"
-name="password"
-class="form-control"
-placeholder="Password">
-
+<input type="password" name="password" class="form-control" placeholder="Password">
 
 <br>
 
 
-<button
-name="login"
-class="btn btn-success w-100">
+<button name="login" class="btn btn-success w-100">
 
 Login
 
 </button>
-
-
-<br><br>
-
-
-<div class="text-center">
-
-
-<a href="register.php">
-
-Create Account
-
-</a>
-
-
-<br>
-
-
-<a href="forgot_password.php">
-
-Forgot Password?
-
-</a>
-
-
-</div>
 
 
 </form>
@@ -198,9 +234,7 @@ Forgot Password?
 
 </div>
 
-
 </div>
-
 
 </body>
 </html>
